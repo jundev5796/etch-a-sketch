f2d151ae19c95ce9a41222cf022868512cff6674
HyunJun Baek <hjbaek5796@gmail.com>
2024-08-10 14:18:22 +0900

    Implement trailing colors on mouseover

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ function createGrid() {
   for (let i = 1; i < 257; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
+
+    grid.addEventListener("mouseover", function () {
+      grid.style.backgroundColor = generateColor();
+    });
+
     container.appendChild(grid);
   }
+}
+
+function generateColor() {
+  const color = Math.floor(Math.random() * 256);
+  return `#${color}`;
 }
 
 createGrid();
